0984efc4bd1019bd0fa4d07dd1ce3ce5740b4a32
kostas lazaros <konlazaros@gmail.com>
2023-03-28 15:06:33 +0300

    dataset model refined

--- dataset/admin.py
+++ dataset/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Dataset
+from .models import Dataset, Disease, Modality
 
 admin.site.register(Dataset)
+admin.site.register(Disease)
+admin.site.register(Modality)
--- dataset/models.py
+++ dataset/models.py
@@ -4,6 +4,17 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.template.defaultfilters import slugify
 
+class Disease(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+class Modality(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 class Dataset(models.Model):
     def file_upload_to(self, instance=None):
@@ -14,6 +25,8 @@ class Dataset(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         get_user_model(), default=1, on_delete=models.SET_DEFAULT)
+    disease = models.ForeignKey(Disease, on_delete=models.PROTECT)
+    modality = models.ForeignKey(Modality, on_delete=models.PROTECT)
     description = models.TextField()
     zipfile = models.FileField(upload_to=file_upload_to, max_length=255)
 
